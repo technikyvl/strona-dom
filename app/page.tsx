fde35b98f121f5516ec0b6736b586c394bac474f
technikyvl <jakub4.biznes@gmail.com>
2025-10-16 21:02:01 +0200

    k

--- app/page.tsx
+++ app/page.tsx
@@ -8,10 +8,9 @@ export default function Home() {
   return (
     <main className="min-h-screen bg-background">
       <HeroSection />
+      <StaggerTestimonials />
       <ToursSection />
       <InspireSection />
-      {/* Testimonials follow after Inspire when gallery is removed */}
-      <StaggerTestimonials />
       <ContactSection />
     </main>
   )
